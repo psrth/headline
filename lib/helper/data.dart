ad56257e958b6891e807b0d95f555d047a7e65bd
Parth Sharma <parthsharma151@gmail.com>
2020-07-22 20:00:19 +0530

    topics tiles

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -12,37 +12,37 @@ List<CategoryModel> getCategories() {
   categoryModel = new CategoryModel();
 
   // 2 — COVID
-  categoryModel.categoryName = "Business";
+  categoryModel.categoryName = "COVID-19";
   categoryModel.imageUrl =
-      "https://images.unsplash.com/photo-1480944657103-7fed22359e1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80";
+      "https://images.unsplash.com/photo-1584521764592-48f33fe7e088?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
   category.add(categoryModel);
   categoryModel = new CategoryModel();
 
   // 3 — Entertainment
   categoryModel.categoryName = "Entertainment";
   categoryModel.imageUrl =
-      "https://images.unsplash.com/photo-1480944657103-7fed22359e1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80";
+      "https://images.unsplash.com/photo-1525869811964-53594bfcb4b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80";
   category.add(categoryModel);
   categoryModel = new CategoryModel();
 
   // 4 — Technology
   categoryModel.categoryName = "Technology";
   categoryModel.imageUrl =
-      "https://images.unsplash.com/photo-1480944657103-7fed22359e1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80";
+      "https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80";
   category.add(categoryModel);
   categoryModel = new CategoryModel();
 
   // 5 — Sports
   categoryModel.categoryName = "Sports";
   categoryModel.imageUrl =
-      "https://images.unsplash.com/photo-1480944657103-7fed22359e1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80";
+      "https://images.unsplash.com/photo-1471295253337-3ceaaedca402?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80";
   category.add(categoryModel);
   categoryModel = new CategoryModel();
 
   // 6 — World Newa
   categoryModel.categoryName = "World News";
   categoryModel.imageUrl =
-      "https://images.unsplash.com/photo-1480944657103-7fed22359e1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80";
+      "https://images.unsplash.com/photo-1584931423298-c576fda54bd2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80";
   category.add(categoryModel);
   categoryModel = new CategoryModel();
 
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:headline/helper/data.dart';
 import 'package:headline/main.dart';
+import 'package:headline/models/catgory_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +11,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = new List<CategoryModel>();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +27,35 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text("Headline"),
-            Text("News", style: TextStyle(color: Colors.blue))
+            Text("News",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w700,
+                ))
           ],
         ),
         centerTitle: true,
-        elevation: 0.5,
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              height: 70,
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CategoryTile(
+                      imageUrl: categories[index].imageUrl,
+                      categoryName: categories[index].categoryName,
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -29,13 +63,59 @@ class _HomeState extends State<Home> {
 
 class CategoryTile extends StatelessWidget {
   @override
-  final imageUrl, categoryName;
-  CategoryTile({this.imageUrl, this.categoryName});
+  final String imageUrl, categoryName;
+  CategoryTile({@required this.imageUrl, this.categoryName});
 
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(
+                imageUrl,
+                width: 120,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 120,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.black26,
+              ),
+              child: Text(
+                categoryName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+  BlogTile(
+      {@required this.imageUrl, @required this.title, @required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Stack(
-        children: <Widget>[Image.network(imageUrl, width: 120, height: 60)],
+      child: Column(
+        children: <Widget>[Image.network(imageUrl), Text(title), Text(desc)],
       ),
     );
   }
