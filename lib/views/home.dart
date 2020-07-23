3997c6ef8c013c419467759678e75e7c94661a72
Parth Sharma <parthsharma151@gmail.com>
2020-07-23 11:30:58 +0530

    added webview for redirects

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,6 +5,7 @@ import 'package:headline/helper/news.dart';
 import 'package:headline/main.dart';
 import 'package:headline/models/article_model.dart';
 import 'package:headline/models/catgory_model.dart';
+import 'package:headline/views/article_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -86,9 +87,11 @@ class _HomeState extends State<Home> {
                             itemCount: articles.length,
                             itemBuilder: (context, index) {
                               return BlogTile(
-                                  imageUrl: articles[index].urlToImage,
-                                  title: articles[index].title,
-                                  desc: articles[index].description);
+                                imageUrl: articles[index].urlToImage,
+                                title: articles[index].title,
+                                desc: articles[index].description,
+                                url: articles[index].url,
+                              );
                             }),
                       ),
                     ],
@@ -146,41 +149,50 @@ class CategoryTile extends StatelessWidget {
 }
 
 class BlogTile extends StatelessWidget {
-  final String imageUrl, title, desc;
+  final String imageUrl, title, desc, url;
   BlogTile(
-      {@required this.imageUrl, @required this.title, @required this.desc});
+      {@required this.imageUrl,
+      @required this.title,
+      @required this.desc,
+      @required this.url});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 20),
-      child: Column(
-        children: <Widget>[
-          ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.network(imageUrl)),
-          SizedBox(
-            height: 8,
-          ),
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black87,
-              fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => ArticleView(blogUrl: url)));
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 20),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(imageUrl)),
+            SizedBox(
+              height: 8,
             ),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Text(
-            desc,
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.black54,
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black87,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              desc,
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.black54,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
